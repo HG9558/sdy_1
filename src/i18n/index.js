c0a403cb1b8d9b034cba8365284896a790cfe18c
alan <hg9558@126.com>
2024-01-04 12:02:10 +0800

    Update index.js

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,33 +1,8 @@
-import Vue from "vue";
-import VueI18n from "vue-i18n";
-import state from "../store/global/state";
-import messages from "./messages";
-const { crc32 } = require("crc");
-Vue.use(VueI18n);
-
-export const { language } = state
-
-localStorage.setItem('language', language)
-
-const i18n = new VueI18n({
-  locale: language,
-  fallbackLocale: language,
-  messages,
-});
-
-export function lang(key) {
-  let hashKey = `k${crc32(key).toString(16)}`;
-  let words = i18n.t(hashKey);
-  if (words === hashKey) {
-    words = key;
-    console.log(key, "-未翻译");
-  }
-  return words;
-}
-
-// 单独声明一个函数用于格式化翻译 不进行扫描
-export function _lang(key, values) {
-  return i18n.t(key, values);
-}
-
-export default i18n;
+"k-2de511f1": "删除当前记录",
+  "k-5a8b9e72": "红牌",
+  "k2747d9ac": "黄牌",
+  "k-40fe3e01": "是否确认删除数据么",
+  "k-6d916cc1": "188数据源联赛ID",
+  "k329a5ff8": "联赛LOGO",
+  "k60442504": "联赛中文名",
+  "k2d4c7434": "联赛英文名"
